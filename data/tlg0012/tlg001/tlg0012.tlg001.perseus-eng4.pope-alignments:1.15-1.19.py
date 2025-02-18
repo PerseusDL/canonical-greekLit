e926452ce08269207214b9bdc2bf57dcafbd1e4f
miagia27 <mgiatr01@tufts.edu>
2025-02-18 15:28:43 -0500

    Create tlg0012.tlg001.perseus-eng4.pope-alignments:1.15-1.19.py

---
+++ data/tlg0012/tlg001/tlg0012.tlg001.perseus-eng4.pope-alignments:1.15-1.19.py
@@ -0,0 +1,11 @@
+alignments = {
+  "1.15-1.19": {
+    "butler":"""and he besought the Achaeans, but most of all the two sons of Atreus, who were their chiefs. 
+    "Sons of Atreus," he cried, "and all other Achaeans, may the gods who dwell in <placeName key="tgn,7011019"
+    >Olympus</placeName> grant you to sack the city of Priam, and to reach your homes in safety;
+    "pope": """He said: and reverent to the Pleiades, He raised his arms, and thus address’d the chiefs:
+'Ye sons of Atreus, and ye other chiefs, May the gods, who live in Olympus, bless 
+Your arms with conquest, and your return with joy! But for my daughter, give her back with speed,
+And let me part with ransom to redeem. My captive child."""
+    }
+}
